12f86fdbd83f0031891ead44c39782cad8970827
Wesley Tessaro <wesley.andrade@novaandradina.org>
2022-03-01 14:31:43 -0400

    First Version, working

---
+++ index.js
@@ -0,0 +1,72 @@
+function insertOnTable1(coluna1, coluna2) {
+  tabela = document.getElementById("tabela1");
+  linha = tabela.insertRow(-1);
+  coluna1_ = linha.insertCell(0);
+  coluna2_ = linha.insertCell(1);
+  coluna1_.innerHTML = coluna1;
+  coluna2_.innerHTML = coluna2;
+}
+
+function insertOnTable2(distancia, resistencia, umidade) {
+  tabela = document.getElementById("tabela2");
+  linha = tabela.insertRow(-1);
+  coluna1_ = linha.insertCell(0);
+  coluna2_ = linha.insertCell(1);
+  coluna3_ = linha.insertCell(2);
+  coluna4_ = linha.insertCell(3);
+  coluna5_ = linha.insertCell(4);
+  coluna1_.innerHTML = distancia;
+  coluna2_.innerHTML = resistencia;
+  coluna3_.innerHTML = umidade;
+  coluna4_.innerHTML = "";
+  coluna5_.innerHTML = "";
+}
+
+function ler(input) {
+  let file = input.files[0];
+  let reader = new FileReader();
+  reader.readAsText(file);
+
+  reader.onload = function () {
+    console.log(reader.result);
+    let jsonArquivos = JSON.parse(reader.result);
+    //Recupera as informações do campo dos formulários
+    tabela = document.getElementById("tabela1");
+    tabela.innerHTML = "";
+    tratamento = document.getElementById("tratamento").value;
+    transecao = document.getElementById("transecao").value;
+    distancia = document.getElementById("distancia").value;
+    insertOnTable1("Tratamento", tratamento);
+    insertOnTable1("Transeção", transecao);
+    insertOnTable1("Tratamento", tratamento);
+    insertOnTable1("Distância", distancia);
+    insertOnTable1("ID", file.name);
+    insertOnTable1("Latitude:", jsonArquivos["latitude"]);
+    insertOnTable1("Longitude:", jsonArquivos["longitude"]);
+    insertOnTable1("Horário Coleta:", jsonArquivos["hora"]);
+
+    for (let i = 0; i < jsonArquivos["umidade"].length; i++) {
+      insertOnTable2(jsonArquivos["distancia"][i],jsonArquivos["resistencia"][i],jsonArquivos["umidade"][i]);
+    }
+
+    /*
+    alert("codigo:"+codigo);
+    alert("nome:"+codigo);
+    //Encontra o elemento tabela
+    tabela = document.getElementById("tabela1");
+    linha = tabela.insertRow(-1);
+    coluna1 = linha.insertCell(0);
+    coluna2 = linha.insertCell(1);
+
+    //Inclui o valor do campo do formulário em sua respectiva coluna
+    coluna1.innerHTML = codigo;
+    coluna2.innerHTML = nome;
+ */
+
+    console.log(jsonArquivos["umidade"][0]);
+  };
+
+  reader.onerror = function () {
+    console.log(reader.error);
+  };
+}
